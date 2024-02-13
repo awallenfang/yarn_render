f17be4e82e1ac171288c63e3bc0ea64ef70ab061
RitzGit <marvin@wallenfang.de>
2024-02-13 12:40:24 +0100

    Prerendered image outputs

--- render.py
+++ render.py
@@ -11,32 +11,36 @@ from mitsuba_modules.bundle_bsdf import BundleBSDF
 import numpy as np
 import colour
 
-mi.set_variant('llvm_mono')
 
-images = np.zeros((1000, 1000, 3, 11))
+# mi.set_variant('llvm_mono')
 
-mi.register_bsdf('bundle_bsdf', lambda props: BundleBSDF(props))
-# print("Successfully registered integrator")
+# images = np.zeros((1000, 1000, 3, 11))
 
-# scene = mi.load_file("scenes/tea_cozy_scene.xml")
-# scene = mi.load_dict(mi.cornell_box())
+# mi.register_bsdf('bundle_bsdf', lambda props: BundleBSDF(props))
+# # print("Successfully registered integrator")
 
-# Load the scene file into a string and replace WAVELENGTH with the wavelength
-scene = ""
-with open("inside_test.xml", "r") as file:
-    scene = file.read()
+# # scene = mi.load_file("scenes/tea_cozy_scene.xml")
+# # scene = mi.load_dict(mi.cornell_box())
 
-for w in range(400, 710, 300//10):
-    print("Working on wavelength: ", w)
-    scene = ""
-    with open("inside_test.xml", "r") as file:
-        scene = file.read()
-    scene = scene.replace("WAVELENGTH", str(float(w)))
-    scene_obj = mi.load_string(scene)
-    image = mi.render(scene_obj).numpy()
-    images[:, :, 0, (w - 400) // (300//10)] = image.reshape(1000,1000)
-    images[:, :, 1, (w - 400) // (300//10)] = image.reshape(1000,1000)
-    images[:, :, 2, (w - 400) // (300//10)] = image.reshape(1000,1000)
+# # Load the scene file into a string and replace WAVELENGTH with the wavelength
+# scene = ""
+# with open("inside_test.xml", "r") as file:
+#     scene = file.read()
+
+# for w in range(400, 710, 300//10):
+#     print("Working on wavelength: ", w)
+#     scene = ""
+#     with open("inside_test.xml", "r") as file:
+#         scene = file.read()
+#     scene = scene.replace("WAVELENGTH", str(float(w)))
+#     scene_obj = mi.load_string(scene)
+#     image = mi.render(scene_obj).numpy()
+#     images[:, :, 0, (w - 400) // (300//10)] = image.reshape(1000,1000)
+#     images[:, :, 1, (w - 400) // (300//10)] = image.reshape(1000,1000)
+#     images[:, :, 2, (w - 400) // (300//10)] = image.reshape(1000,1000)
+
+#     # Save the images to a folder for further use
+#     np.save("images/inside_test_" + str(w), image)
 
 # scene = scene.replace("WAVELENGTH", "400.")
 
@@ -50,29 +54,34 @@ for w in range(400, 710, 300//10):
 # plt.imshow(images[:,:,2] ** (1.0 / 2.2))
 # plt.show()
 
+output = np.zeros((1000, 1000, 3))
+
 # Convert the images to XYZ and then to sRGB
 for w in range(400, 710, 300//10):
+    img = np.load("images/inside_test_" + str(w) + ".npy")
     rgb = colour.XYZ_to_RGB(colour.wavelength_to_XYZ(float(w)), colourspace=colour.models.RGB_COLOURSPACE_sRGB)
     print(rgb)
-    images[:, :, 0, (w - 400) // (300//10)] *= rgb[0]
-    images[:, :, 1, (w - 400) // (300//10)] *= rgb[1]
-    images[:, :, 2, (w - 400) // (300//10)] *= rgb[2]
+    # images[:, :, 0, (w - 400) // (300//10)] *= rgb[0]
+    # images[:, :, 1, (w - 400) // (300//10)] *= rgb[1]
+    # images[:, :, 2, (w - 400) // (300//10)] *= rgb[2]
+
+    output += img * rgb
 
 
 # # Save the result to an array
 # xyz = colour.wavelength_to_XYZ(images)
 # rgb_images = colour.XYZ_to_sRGB(xyz)
 # Additively combine the images
-final_image = np.sum(images, axis=3) / 8.
-plt.imshow(images[:,:,:,3])
-plt.show()
+# final_image = np.sum(output, axis=3) / 8.
+# plt.imshow(output)
+# plt.show()
 
-output = np.zeros((1000, 1000, 3))
+# output = np.zeros((1000, 1000, 3))
 
-for i in range(11):
-    output += images[:,:,:,i]
+# for i in range(11):
+#     output += output[:,:,:,i]
 
-output /= 10.
+output /= 5.
 print(np.min(output), np.max(output))
 plt.imshow(output)
 plt.show()
